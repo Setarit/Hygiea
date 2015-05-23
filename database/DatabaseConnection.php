496138f354716a70a5b4a505e9cad57899a9f5d2
javalSoft <javalsoft@gmail.com>
2015-05-23 16:03:55 +0200

    Database connection

--- database/DatabaseConnection.php
+++ database/DatabaseConnection.php
@@ -6,18 +6,21 @@
  * @author javalsoft
  */
 class DatabaseConnection {
-    private $_connection;    
-    
+    private $_connection;
+    private $_url, $_password, $_database, $_username, $_port;
+
+
     public function __construct($autoLoader) {
         $autoLoader->loadDatabaseFactory();
         $configuration = json_decode(file_get_contents($autoLoader->getDatabaseConfigFile()));
+        $this->_url = $configuration->connection;
+        $this->_password = $configuration->password;
+        $this->_username = $configuration->username; 
+        $this->_port = $configuration->port;
         $this->_processVendor($configuration->vendor);
     }
     
     private function _processVendor($vendorName){
-        if(strlen($vendorName) === 0){
-            throw new Exception("No vendor specified in the database.json file");
-        }
-        
+        $this->_connection = Factory::createFromVendor($vendorName, $this->_url, $this->_username, $this->_password, $this->_database);
     }
 }
--- database/factory/Factory.php
+++ database/factory/Factory.php
@@ -5,10 +5,21 @@
  * @author javalsoft
  */
 class Factory {
-    public static function createFromVendor($vendorName){
+    public static function createFromVendor($vendorName, $url, $username, $password, $db){
         $vendorName = strtoupper($vendorName);
         if($vendorName === 'MONGODB'){
+            $url = Factory::processMongoUrl($url, $username, $password);
+            
             return new MongoClient();
+        }else{
+            throw new Exception("Invalid or no vendor specified in the database.json file");
+        }
+    }
+    
+    public static function processMongoUrl($url, $username, $password) {
+        $url = \str_replace('://', '', $url);//prevent double ://
+        if(strpos($url, 'mongodb://') !== -1){//no mongodb:// in url
+            $url = 'mongodb://'.$url;
         }
     }
 }
